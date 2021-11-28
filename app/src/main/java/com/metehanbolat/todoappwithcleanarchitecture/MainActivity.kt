f6258c70b6898f982fb47a5c31e3c0352e6d3dad
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-28 23:51:28 +0300

    Add and List Fragment UI completed.

--- app/src/main/java/com/metehanbolat/todoappwithcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/todoappwithcleanarchitecture/MainActivity.kt
@@ -2,10 +2,14 @@ package com.metehanbolat.todoappwithcleanarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
---
+++ app/src/main/java/com/metehanbolat/todoappwithcleanarchitecture/fragments/update/UpdateFragment.kt
@@ -0,0 +1,20 @@
+package com.metehanbolat.todoappwithcleanarchitecture.fragments.update
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.metehanbolat.todoappwithcleanarchitecture.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+}
